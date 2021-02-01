c381bef25b36683cd7fb7c0da2e6c317e7f5d3bf
NastuFireDragon <[henry.sean.r@gmail.com]>
2021-01-31 19:58:03 -0600

    Edited the font sizes for tablet, updated the favicon, and removed the unused js files.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Person from './js/test';
-
-const erina = new Person('Erina', 16);
-console.log(erina.getInfo());
-console.log('Asuna is hot too');
-console.log('So is Rias');
-console.log('Don\'t forget about Raphtalia');
